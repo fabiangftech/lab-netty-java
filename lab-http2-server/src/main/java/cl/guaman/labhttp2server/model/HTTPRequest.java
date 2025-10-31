8b1e6862f2ee57640e330ef15cd7c31b84c3bd3e
fabiangftech <fabian.gf.tech@gmail.com>
2025-10-31 16:02:30 -0300

    chore: add frame from http2

--- lab-http2-server/src/main/java/cl/guaman/labhttp2server/model/HTTPRequest.java
+++ lab-http2-server/src/main/java/cl/guaman/labhttp2server/model/HTTPRequest.java
@@ -1,28 +1,40 @@
 package cl.guaman.labhttp2server.model;
 
+import io.netty.buffer.ByteBuf;
 import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.HttpHeaders;
+import io.netty.handler.codec.http2.Http2Headers;
+import io.netty.handler.codec.http2.Http2HeadersFrame;
 
 public class HTTPRequest {
 
     private final HTTPVersion version;
-    private FullHttpRequest fullHttpRequest;
+    private HttpHeaders headers;
+    private Http2Headers http2Headers;
+    private final byte[] body;
 
     public HTTPRequest(FullHttpRequest fullHttpRequest) {
         this.version = HTTPVersion.V1_1;
-        this.fullHttpRequest = fullHttpRequest;
+        this.headers = fullHttpRequest.headers();
+        this.body = fullHttpRequest.content().array();
+    }
+
+    public HTTPRequest(Http2HeadersFrame headersFrame, ByteBuf body) {
+        this.version = HTTPVersion.V2_0;
+        this.http2Headers = headersFrame.headers();
+        this.body = body.array();
     }
 
     public String getHeader(String key) {
         if (HTTPVersion.V1_1.equals(version)) {
-            return fullHttpRequest.headers().get(key);
+            return headers.get(key);
+        } else if (HTTPVersion.V2_0.equals(version)) {
+            return http2Headers.get(key).toString();
         }
         throw new IllegalArgumentException(String.format("version not supported %s", version));
     }
 
     public byte[] getBody() {
-        if (HTTPVersion.V1_1.equals(version)) {
-            return fullHttpRequest.content().array();
-        }
-        throw new IllegalArgumentException(String.format("version not supported %s", version));
+        return body;
     }
 }
