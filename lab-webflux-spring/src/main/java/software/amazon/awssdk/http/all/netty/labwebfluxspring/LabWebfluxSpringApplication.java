38a50e73883d9751aa406c30c84ab7f4063bf0f8
fabiangftech <fabian.gf.tech@gmail.com>
2025-11-27 17:35:25 -0300

    chore: add webflux module

---
+++ lab-webflux-spring/src/main/java/software/amazon/awssdk/http/all/netty/labwebfluxspring/LabWebfluxSpringApplication.java
@@ -0,0 +1,13 @@
+package software.amazon.awssdk.http.all.netty.labwebfluxspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabWebfluxSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabWebfluxSpringApplication.class, args);
+    }
+
+}
---
+++ lab-webflux-spring/src/test/java/software/amazon/awssdk/http/all/netty/labwebfluxspring/LabWebfluxSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package software.amazon.awssdk.http.all.netty.labwebfluxspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabWebfluxSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
